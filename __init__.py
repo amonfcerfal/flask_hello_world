62e70be56b84e35283571e111af851ada0e87984
amonfcerfal <129786146+amonfcerfal@users.noreply.github.com>
2024-03-20 15:26:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -79,32 +79,6 @@ def enregistrer_client():
 
 import sqlite3
 
-connection = sqlite3.connect('database.db')
-
-with open('schema.sql') as f:
-    connection.executescript(f.read())
-
-cur = connection.cursor()
-
-cur.execute("INSERT INTO livres (title, auteur) VALUES (?, ?)",
-            ('Moby-Dick', 'Herman Melville')
-            )
-
-cur.execute("INSERT INTO livres (title, auteur) VALUES (?, ?)",
-            ('Notre-Dame de Paris', 'Victor Hugo')
-            )
-
-cur.execute("INSERT INTO livres (title, auteur) VALUES (?, ?)",
-            ('CosmoZ', 'Christophe Claro')
-            )
-
-cur.execute("INSERT INTO livres (title, auteur) VALUES (?, ?)",
-            ('La reine du crime', 'Agatha Christie')
-            )
-
-
-connection.commit()
-connection.close()
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
