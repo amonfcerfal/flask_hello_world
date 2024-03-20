0e7359266d2ccfeadc8b38165b4bbe9436c0c558
amonfcerfal <129786146+amonfcerfal@users.noreply.github.com>
2024-03-20 16:00:26 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,28 +1,31 @@
 DROP TABLE IF EXISTS clients;
-CREATE TABLE clients (
+DROP TABLE IF EXISTS livres;
+DROP TABLE IF EXISTS emprunts;
+DROP TABLE IF EXISTS utilisateurs;
+
+CREATE TABLE utilisateurs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    nom TEXT NOT NULL,
-    prenom TEXT NOT NULL,
-    adresse TEXT NOT NULL
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'utilisateur'
 );
 
-CREATE TABLE IF NOT EXISTS livres (
+CREATE TABLE livres (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titre TEXT NOT NULL,
     auteur TEXT NOT NULL,
-    annee_publication INTEGER,
-    disponible BOOLEAN DEFAULT TRUE
+    annee INTEGER,
+    disponible BOOLEAN NOT NULL DEFAULT 1
 );
 
-CREATE TABLE IF NOT EXISTS emprunts (
+CREATE TABLE emprunts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    livre_id INTEGER NOT NULL,
     utilisateur_id INTEGER NOT NULL,
+    livre_id INTEGER NOT NULL,
     date_emprunt DATE NOT NULL,
     date_retour DATE,
-    FOREIGN KEY (livre_id) REFERENCES livres (id),
-    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id)
+    FOREIGN KEY(utilisateur_id) REFERENCES utilisateurs(id),
+    FOREIGN KEY(livre_id) REFERENCES livres(id)
 );
 
 ALTER TABLE utilisateurs ADD COLUMN role TEXT DEFAULT 'utilisateur';
