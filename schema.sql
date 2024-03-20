0f0512cd5d58667ffc608e84280f7596cb8ec575
amonfcerfal <129786146+amonfcerfal@users.noreply.github.com>
2024-03-20 15:38:40 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,3 +6,23 @@ CREATE TABLE clients (
     prenom TEXT NOT NULL,
     adresse TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS livres (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre TEXT NOT NULL,
+    auteur TEXT NOT NULL,
+    annee_publication INTEGER,
+    disponible BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS emprunts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    livre_id INTEGER NOT NULL,
+    utilisateur_id INTEGER NOT NULL,
+    date_emprunt DATE NOT NULL,
+    date_retour DATE,
+    FOREIGN KEY (livre_id) REFERENCES livres (id),
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id)
+);
+
+ALTER TABLE utilisateurs ADD COLUMN role TEXT DEFAULT 'utilisateur';
